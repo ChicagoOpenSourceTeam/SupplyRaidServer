020c8a11a2d22a1147d8221acbeaa17920a4e27b
IanSwift <ian.p.swift@gmail.com>
2016-08-30 17:46:33 -0500

    Deliver script finished.

--- deliver.sh
+++ deliver.sh
@@ -1,21 +1,20 @@
 #!/bin/sh
 
-if [ "$(git pull origin master)" ]; then
+if !(git pull origin master); then
 	echo Failed to pull
-	exit -1
+	exit 1
 fi
-
-if [ "$(./gradlew test)" ]; then
+if !(./gradlew test); then
 	echo Please fix tests before delivering
-	exit -1
+	exit 1
 fi
-if [ "$(git push origin master)" ]; then
+if !(git push origin master); then
 	echo Failed to push git repository
-	exit -1
+	exit 1
 fi
-if [ "$(git push heroku master)" ]; then
+if !(git push heroku master); then
 	echo Failed to deploy to heroku
-	exit -1;
+	exit 1;
 fi
 echo Deploy successful!
 exit 0;
