317ef7d4f3c133166b098921707d31f0c459aeb4
JmBirk <JmBirk@gmail.com>
2016-09-19 07:48:39 -0500

    Fixed deletion

--- src/main/java/org/cost/game/GameRepository.java
+++ src/main/java/org/cost/game/GameRepository.java
@@ -2,7 +2,10 @@ package org.cost.game;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface GameRepository extends JpaRepository<Game, String> {
+
+    boolean deleteByGameName(String gameName);
 }
