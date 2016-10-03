f65ed45faf9f55ca741c0f3d1abd732994d6071a
IanSwift <ian.p.swift@gmail.com>
2016-10-02 21:09:50 -0500

    Add SpringHttpSession

--- src/main/java/org/cost/CORSConfig.java
+++ src/main/java/org/cost/CORSConfig.java
@@ -3,6 +3,8 @@ package org.cost;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.session.MapSessionRepository;
+import org.springframework.session.config.annotation.web.http.EnableSpringHttpSession;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 import org.springframework.session.web.http.HeaderHttpSessionStrategy;
 import org.springframework.session.web.http.HttpSessionStrategy;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -12,6 +14,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 
 @Configuration
 @EnableWebMvc
+@EnableSpringHttpSession
 public class CORSConfig extends WebMvcConfigurerAdapter {
 
     @Override
