16a3e25720196fd5843f60ff48260e70438a4f41
IanSwift <ian.p.swift@gmail.com>
2016-10-02 22:19:45 -0500

    fix broken build

--- src/main/java/org/cost/CORSConfig.java
+++ src/main/java/org/cost/CORSConfig.java
@@ -1,12 +1,6 @@
 package org.cost;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.session.MapSessionRepository;
-import org.springframework.session.config.annotation.web.http.EnableSpringHttpSession;
-import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
-import org.springframework.session.web.http.HeaderHttpSessionStrategy;
-import org.springframework.session.web.http.HttpSessionStrategy;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
